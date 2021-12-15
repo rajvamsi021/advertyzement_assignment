4c370121e6289dc651740eb31965ee80496b241a
Rajvamsi Srigakulam <rajvamsi021@gmail.com>
2021-12-15 12:43:59 +0530

    added src

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React,{useState} from "react";
+
+import Nav from './components/Nav';
+import Body from './components/Body';
+
+function App() {
+
+  const [drive, setDrive] = useState();
+
+  const sendDataToParent = (index) => { 
+    // console.log(index);
+    setDrive(index);
+  };
+
+
+  return (
+    <div>
+      <Nav sendDataToParent={sendDataToParent}/>
+      {drive ? <Body /> : <div className="actual">Please click Get Users to find the details of the Users.</div>}
+    </div>
+  );
+}
+
+export default App;
